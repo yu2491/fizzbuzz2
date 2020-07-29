58f4486d1ec84ec0e9bba356548958674d4296f3
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-29 11:14:53 +0100

    finished fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 5 == 0 && number % 3 == 0
     return 'fizzbuzz'
-  elsif number == 3
+  elsif number % 3 == 0
     return 'fizz'
-  elsif number == 5
+  elsif number % 5 == 0
     return 'buzz'
   else
-    return 2
+    return number
   end
 end
