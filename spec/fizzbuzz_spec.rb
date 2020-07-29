a1e659c9788d8620366a6c7632b3a3955e2b6d46
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-07-29 10:04:56 +0000

    tests ammended

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when passed multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-  it  'returns 2 when passed 2' do
+  it  'returns number when number cannot be multiplied by 3 or 5' do
     expect(fizzbuzz(2)).to eq 2
   end
 end
